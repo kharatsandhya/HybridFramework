1611e841b9289f1c0898f3d31a532cd2750ddce5
skharat1 <kharatsandhya@gmail.com>
2021-07-25 15:58:01 +0530

    initial commit

---
+++ src/main/test/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,48 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.base.TestBase;
+import com.qa.pages.HomePage;
+import com.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+	LoginPage loginPage = new LoginPage();
+	HomePage homePage;
+
+	public LoginPageTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		loginPage = new LoginPage();
+	}
+
+//	@AfterMethod()
+//	public void tearDown() {
+//		driver.quit();
+//	}
+
+	@Test(priority = 1, enabled = false)
+	public void loginPagetitleTest() {
+		String title = loginPage.validatePageTitle();
+		Assert.assertEquals(title, "#1 Free CRM customer relationship management software cloud");
+	}
+
+	@Test(priority = 2, enabled = false)
+	public void crmLogoImageTest() {
+		boolean flag = loginPage.validateCMRImage();
+		Assert.assertTrue(flag);
+	}
+
+	@Test(priority = 3)
+	public void loginTest() {
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+
+	}
+}
